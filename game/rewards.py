286c37f24c7fe9f2ba4f5b6cbb35fffae2bb8254
Thanasis Tsitos <48286119+ThanasisTs@users.noreply.github.com>
2021-05-26 11:29:28 +0300

    Update rewards.py

--- game/rewards.py
+++ game/rewards.py
@@ -7,43 +7,42 @@ def main(config):
 
 def reward_function_maze(done, timedout, goal=None):
 	if reward_type in ["Timeout", "timeout"]:
-		return reward_function_timeout_pentalty(done, timedout)
+		return reward_function_timeout_penalty(goal_reached, timedout)
 	elif reward_type in ["Distance", "distance"]:
-		return reward_function_distance(done, timedout, goal=goal)
+		return reward_function_distance(goal_reached, timedout, goal=goal)
 	elif reward_type in ["Shafti", "shafti"]:
-		return reward_function_shafti(done, timedout)
+		return reward_function_shafti(goal_reached)
 
-def reward_function_timeout_pentalty(done, timedout):
+def reward_function_timeout_penalty(goal_reached, timedout):
 	# for every timestep -1
 	# timed out -50
 	# reach goal +100
-	if done and not timedout:
+	if goal_reached:
 		return 100
 	if timedout:
 		return -50
 	return -1
 
-def reward_function_shafti(done, timedout):
+def reward_function_shafti(goal_reached):
 	# For every timestep -1
 	# Reach goal +100
-	if done:
+	if goal_reached:
 		return 100
-	if timedout:
-		return -1
 	return -1
 
-def reward_function_distance(done, timedout, ball, goal=None):
+def reward_function_distance(goal_reached, timedout, ball, goal=None):
 	# for every timestep -target_distance
 	# timedout -50
 	# reach goal +100
-	if done:
+	if goal_reached:
 		return 100
 	if timedout:
 		return -50
-	target_distance = get_distance_from_goal(ball, goal)
+	return get_distance_from_goal(ball, goal)
+	
 
-def reward_function(timedout, goal=None):
-	if done:
+def reward_function(goal_reached, timedout, goal=None):
+	if goal_reached:
 		return 100
 	# Construct here the mathematical reward function
 	# The reward function can depend on time, the distance of 
